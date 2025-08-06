7c103b1d4054a1c4294eb8d28cded97c8af7e341
Sasha <kluzodota@gmail.com>
2025-08-06 13:31:14 +0300

    conected JWT tockens

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from books.models import Book
 from books.serializers import BookSerializer
@@ -8,3 +9,4 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
