891eba50e5255a19cf8e7aa34909bf187a334a01
Sasha <kluzodota@gmail.com>
2025-08-06 11:43:20 +0300

    created Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+
+    class CoverChoices(models.TextChoices):
+        HARD = "HARD", "Hard"
+        SOFT = "SOFT", "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverChoices.choices,
+        default=CoverChoices.SOFT,
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0),
+        ],
+    )
+
+    def __str__(self):
+        return f"self.title by {self.author}"
