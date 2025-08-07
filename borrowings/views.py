2e51b7b80f7f68e728be529fa47734a80bf548ae
Sasha <kluzodota@gmail.com>
2025-08-07 11:32:58 +0300

    made it so that users could only see their borrowings, and admins could see all borrowings

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer
 
 
-class BorrowingsViewSet(viewsets.ModelViewSet):
-    queryset = Borrowing.objects.all()
+class BorrowingsViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        request = self.request
+        queryset = self.queryset
+        user = request.user
+
+        if user.is_staff:
+            return queryset
+
+        return queryset.filter(user=user)
