7c50192d091b9d7473c38d24382ef70aac6b642c
Sasha <kluzodota@gmail.com>
2025-08-07 17:20:55 +0300

    created servise.py for sending messages

---
+++ notifications/services.py
@@ -0,0 +1,25 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def send_telegram_message(message: str) -> None:
+    if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
+        return None
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message, "parse_mode": "HTML"}
+
+    try:
+        requests.post(url, data=payload, timeout=10)
+    except requests.RequestException as e:
+        print(f"Telegram error: {e}")
+
+
+if __name__ == "__main__":
+    send_telegram_message("Hello world!")
