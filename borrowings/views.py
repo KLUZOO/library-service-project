803e27325cfb7814eedfcd1dc142caafab7d1ecc
Sasha <kluzodota@gmail.com>
2025-08-07 10:05:55 +0300

    added BorrowingCreateViewSet for Borrowing

--- borrowings/views.py
+++ borrowings/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer
+from borrowings.serializers import BorrowingSerializer, BorrowingCreateViewSet
 
 
 class BorrowingsViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingSerializer
+        return BorrowingCreateViewSet
