8967d7aaacb755ffb19ffd4b070f097ebb8fe1e6
Sasha <kluzodota@gmail.com>
2025-08-07 11:14:11 +0300

    added the ability to return books

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,9 +1,13 @@
 from django.db import transaction
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from django.utils import timezone
+from rest_framework import generics, viewsets, status
+from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
+from books.models import Book
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer, BorrowingCreateViewSet
 
@@ -28,3 +32,32 @@ class BorrowingsViewSet(viewsets.ModelViewSet):
             book.save()
 
             serializer.save(user=self.request.user)
+
+    @action(
+        detail=True,
+        methods=["post"],
+        permission_classes=[IsAuthenticated],
+        url_path="return",
+    )
+    def return_act(self, request, pk=None):
+        with transaction.atomic():
+            borrowing = self.get_object()
+
+            if request.user != borrowing.user:
+                raise ValidationError("You do not have access to this loan.")
+
+            if borrowing.actual_return_date:
+                raise ValidationError("This book has already been returned.")
+
+            borrowing.actual_return_date = timezone.now()
+            borrowing.save()
+
+            book = borrowing.book
+            book.inventory += 1
+            book.save()
+
+            serializer = BorrowingSerializer(borrowing)
+            return Response(
+                {"message": "Book returned successfully.", "data": serializer.data},
+                status=status.HTTP_200_OK,
+            )
